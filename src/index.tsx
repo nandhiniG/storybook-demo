be100d01deed4807b1e47da3e81ffd9bb369dc97
nandhiniG <nandhini.govind@anywhere.co>
2024-02-20 20:10:41 +0530

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ProductComponent from "./ProductComponent/ProductComponent";
-import ProductGrid from "./ProductGrid/ProductGrid";
-import productData from "./data/productData";
+
+import TimePicker from "./TimePicker/TimePicker";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <ProductComponent name="2GB Monthly" description="Validity 6 Months* - Pay 3 months and get 3 months free" id="sim1"/>
-      <ProductGrid products={productData} />
+     <TimePicker timeFormat='12' id={'test'} onChange={function (time: number): void {
+      throw new Error('Function not implemented.');
+    } }></TimePicker>
   </React.StrictMode>
 );
